f3e23a0574bfb3fd17da750b283a8dac329dd397
zehao99 <zehao99@163.com>
2020-08-13 22:39:23 +0900

    Updates

---
+++ precache-manifest.4b0a9d1ecc39e8cf4d7a1d79c4cf770d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30fff43b95d02f8e54fc4faa973357b2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b2867fabd1130eb224e5",
+    "url": "/portfolio/static/css/main.dc267ebe.chunk.css"
+  },
+  {
+    "revision": "3fd5c510d93cf5f784e4",
+    "url": "/portfolio/static/js/2.685e11f0.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/portfolio/static/js/2.685e11f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2867fabd1130eb224e5",
+    "url": "/portfolio/static/js/main.8d6a1b65.chunk.js"
+  },
+  {
+    "revision": "3f435d74e3688edf3465",
+    "url": "/portfolio/static/js/runtime-main.662794fe.js"
+  },
+  {
+    "revision": "f0ccdd25687e9365c2fd23926ce16bc0",
+    "url": "/portfolio/static/media/BackGround.f0ccdd25.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.93127763ea0aac81bf452feb7eb56992.js"
+  "/portfolio/precache-manifest.4b0a9d1ecc39e8cf4d7a1d79c4cf770d.js"
 );
 
 self.addEventListener('message', (event) => {
