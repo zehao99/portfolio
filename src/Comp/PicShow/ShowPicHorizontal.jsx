e713123d59a4d96e3761260062ee32a05eabac71
zehao99 <zehao99@163.com>
2021-12-06 22:13:23 +0900

    Fix annimation for image show

--- src/Comp/PicShow/ShowPicHorizontal.jsx
+++ src/Comp/PicShow/ShowPicHorizontal.jsx
@@ -10,18 +10,36 @@ const variants = {
     return {
       x: direction > 0 ? 1000 : -1000,
       opacity: 0,
+      scale: 0,
+      transition: {
+        // x: { type: 'spring', stiffness: 300, damping: 200,duration: 0.5},
+        // opacity: { duration: 0.2 },
+        duration: 0.5
+      }
     };
   },
   center: {
     zIndex: 1,
     x: 0,
+    scale: 1,
     opacity: 1,
+    transition: {
+      // x: { type: 'spring', stiffness: 300, damping: 200,duration: 0.5},
+      // opacity: { duration: 0.2 },
+      duration: 0.5
+    }
   },
   exit: (direction) => {
     return {
       zIndex: 0,
       x: direction < 0 ? 1000 : -1000,
       opacity: 0,
+      scale: 0,
+      transition: {
+        // x: { type: 'spring', stiffness: 300, damping: 200,duration: 0.5},
+        // opacity: { duration: 0.2 },
+        duration: 0.5
+      }
     };
   },
 };
@@ -73,10 +91,6 @@ const ShowPicHorizontal = (props) => {
                 initial='enter'
                 animate='center'
                 exit='exit'
-                transition={{
-                  x: { type: 'spring', stiffness: 300, damping: 200 },
-                  opacity: { duration: 0.2 },
-                }}
                 drag='x'
                 dragConstraints={{ left: 0, right: 0 }}
                 dragElastic={1}
