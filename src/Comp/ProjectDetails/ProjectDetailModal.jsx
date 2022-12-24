b7a5e934d5b2fcb40f5c34f04357854bb8b04b50
zehao99 <zehaoli99@gmail.com>
2022-12-24 17:15:44 +0900

    Remove unused imports.

--- src/Comp/ProjectDetails/ProjectDetailModal.jsx
+++ src/Comp/ProjectDetails/ProjectDetailModal.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
+import React from 'react';
+import { motion } from 'framer-motion';
 import styles from './ProjectDetailModal.module.scss';
 import Calories from './ModalContents/Calories';
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
